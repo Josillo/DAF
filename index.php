8bdd7b6262db2472478f88cae3e54dbbe0fc3544
Josillo <jose.jaramago101@gmail.com>
2014-02-13 22:14:07 +0000

    index body

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>DAF</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.css" media="all" />
+</head>
+<body>
+	<script src="assets/js/jquery-2.1.0.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
